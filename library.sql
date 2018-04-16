168d8b8f3afedcb1366b38cfc1a61f0c4a07db9c
Darthseid <jared.mcvey@yahoo.com>
2018-04-16 13:57:06 -0500

    Created Customer Table.

--- library.sql
+++ library.sql
@@ -12,9 +12,7 @@ title_id INT(5),
 price decimal(6,2),
 self text,
 compartment text,
-cust_id INT(5),
 borrow_status boolean,
-department_id INT(2),
 
 CONSTRAINT item_pk
 PRIMARY KEY(item_id)
@@ -29,14 +27,25 @@ title_type VARCHAR(11),
 title_id INT(5),
 page_number INT(6),
 copy_amount INT(4),
-publisher_id INT(5),
-author_id INT(5),
-reservation_id INT(5),
 
 CONSTRAINT title_pk
 PRIMARY KEY(title_id),
  CONSTRAINT fk_item0_id FOREIGN KEY(item_id)
 REFERENCES Item(item_id)	
+);
+
+DROP TABLE IF EXISTS Customer;
+CREATE TABLE Customer
+(
+address VARCHAR(33),
+full_name VARCHAR(29),
+borrow_count INT(1),
+reservation_spot INT(2),
+cust_id INT(5),
+
+
+CONSTRAINT customer_pk
+PRIMARY KEY(cust_id) 
 );
 
 DROP TABLE IF EXISTS Borrowing;
@@ -46,16 +55,9 @@ borrowing_id INT(5),
 record VARCHAR(70),
 borrowing_date date,
 return_date date,
-page_number INT(6),
-copy_amount INT(4),
-publisher_id INT(5),
-author_id INT(5),
-reservation_id INT(5),
-department_id INT(2),
 
 CONSTRAINT borrowing_pk
 PRIMARY KEY(borrowing_id) 
-/*add a foreign key to department id and reservation id after all tables are created using an update command */
 );
 
 DROP TABLE IF EXISTS Reservation;
@@ -73,6 +75,7 @@ PRIMARY KEY(reservation_id) ,
 CONSTRAINT fk_item1_id FOREIGN KEY(item_id)
 REFERENCES Item(item_id),
 CONSTRAINT fk_borrowing_id FOREIGN KEY(borrowing_id)
-REFERENCES Borrowing(borrowing_id)		
-/*add a foreign key to customer id after all tables are created using an update command */
+REFERENCES Borrowing(borrowing_id),		
+CONSTRAINT fk_customer0_id FOREIGN KEY(cust_id)
+REFERENCES Customer(cust_id)
 );
