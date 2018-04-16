ea5cfb19de4552ae43f77c5afc011b3cb97ba113
Darthseid <jared.mcvey@yahoo.com>
2018-04-16 12:36:30 -0500

    Added Item table.

--- library.sql
+++ library.sql
@@ -1,3 +1,19 @@
-DROP DATABASE IF EXISTS library;
-CREATE DATABASE library;
-use library;
+DROP DATABASE IF EXISTS Final_Library;
+CREATE DATABASE IF NOT EXISTS Final_Library;
+use Final_Library;
+
+CREATE TABLE Item
+(
+item_id INT(6),
+date_of_purchase date,
+reservation_status boolean,
+title_id INT(5),
+price decimal(6,2),
+self text,
+compartment text,
+cust_id INT(5),
+borrow_status boolean,
+
+CONSTRAINT item_pk
+PRIMARY KEY(item_id)
+);
