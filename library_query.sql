74a9e0fb7204c4405c1bf3d6be5ef6bf65a17be0
Darthseid <jared.mcvey@yahoo.com>
2018-04-17 21:18:48 -0500

    Added subquery.

--- library_query.sql
+++ library_query.sql
@@ -7,3 +7,9 @@ SELECT Author.author_name, Title.title_name, Title.title_type
 FROM Author
 INNER JOIN Title ON Author.title_id = Title.title_id;
 
+SELECT Count(author_id)
+FROM Book
+WHERE publishing_date < (SELECT publishing_date
+FROM Book
+WHERE title_name = 'Moneyball')
+; 
