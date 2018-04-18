e1e71129a966dae6bfd0138a4d78d4672485b342
Darthseid <jared.mcvey@yahoo.com>
2018-04-17 20:49:36 -0500

    Added inner join query.

--- library_query.sql
+++ library_query.sql
@@ -2,3 +2,8 @@ use Final_Library;
 
 SELECT AVG(price)
 FROM Item;
+
+SELECT Author.author_name, Title.title_name, Title.title_type
+FROM Author
+INNER JOIN Title ON Author.title_id = Title.title_id;
+
