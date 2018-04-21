a452979eacd92a25c4a2c3a26ef6e9a71f6213d5
Darthseid <jared.mcvey@yahoo.com>
2018-04-21 01:09:32 -0500

    Added Medium type data

--- sample_library_data.sql
+++ sample_library_data.sql
@@ -45,47 +45,47 @@ VALUES(997799,'2015-09-02',1,55.21,0,'Top Shelf', 0);
 
 
 INSERT INTO Title
-VALUES(973722,'American Gods','Book',1561,645321,6);
+VALUES(973722,'American Gods','Media',1561,645321,6);
 INSERT INTO Title
-VALUES(733112,'One Second After','Book',86380,543, 7);
+VALUES(733112,'One Second After','Media',86380,543, 7);
 INSERT INTO Title
-VALUES(753749,'Wonder Woman','Movie',17810,NULL,2);
+VALUES(753749,'Wonder Woman','Media',17810,NULL,2);
 INSERT INTO Title
-VALUES(307366,'A Farewell To Arms','Book',38735,98, 5);
+VALUES(307366,'A Farewell To Arms','Media',38735,98, 5);
 INSERT INTO Title
-VALUES(52062,'What is the What','Book',43958,392, 3);
+VALUES(52062,'What is the What','Media',43958,392, 3);
 INSERT INTO Title
-VALUES(609672,'The Last of the Mohicans','Book',49471,255,8);
+VALUES(609672,'The Last of the Mohicans','Media',49471,255,8);
 INSERT INTO Title
-VALUES(243139,'The Shawshank Redemption','Movie',59500,NULL, 1);
+VALUES(243139,'The Shawshank Redemption','Media',59500,NULL, 1);
 INSERT INTO Title
-VALUES(296026,'Major League','Movie',63678,NULL, 4);
+VALUES(296026,'Major League','Media',63678,NULL, 4);
 INSERT INTO Title
-VALUES(715810,'A Few Good Men','Movie',63815,NULL, 1);
+VALUES(715810,'A Few Good Men','Media',63815,NULL, 1);
 INSERT INTO Title
-VALUES(252663,'Lawrence of Arabia','Movie',5095,NULL, 3);
+VALUES(252663,'Lawrence of Arabia','Media',5095,NULL, 3);
 INSERT INTO Title
-VALUES(887696,'Ben-Hur','Movie',29219,NULL, 11);
+VALUES(887696,'Ben-Hur','Media',29219,NULL, 11);
 INSERT INTO Title
-VALUES(884303,'Stanford Prison Experiment','Movie',98406,NULL, 1);
+VALUES(884303,'Stanford Prison Experiment','Media',98406,NULL, 1);
 INSERT INTO Title
-VALUES(880124,'The Good, The Bad, and The Ugly','Movie',93297,NULL, 5);
+VALUES(880124,'The Good, The Bad, and The Ugly','Media',93297,NULL, 5);
 INSERT INTO Title
-VALUES(594280,'Amistad','Movie',295,NULL, 1);
+VALUES(594280,'Amistad','Media',295,NULL, 1);
 INSERT INTO Title
-VALUES(801946,'Blues Brothers','Movie',87771,NULL, 6);
+VALUES(801946,'Blues Brothers','Media',87771,NULL, 6);
 INSERT INTO Title
-VALUES(36325,'Murder in Mesopotamia','Book',84035,56, 35);
+VALUES(36325,'Murder in Mesopotamia','Media',84035,56, 35);
 INSERT INTO Title
-VALUES(916506,'Heart of Darkness','Book',43806,176, 24);
+VALUES(916506,'Heart of Darkness','Media',43806,176, 24);
 INSERT INTO Title
-VALUES(826524,'Moneyball','Book',16369,208, 2);
+VALUES(826524,'Moneyball','Media',16369,208, 2);
 INSERT INTO Title
-VALUES(887050,'How to Win Friends and Influence People','Book',1446,127, 17);
+VALUES(887050,'How to Win Friends and Influence People','Media',1446,127, 17);
 INSERT INTO Title
-VALUES(952173,'Orthodoxy','Book',39953,188, 1);
+VALUES(952173,'Orthodoxy','Media',39953,188, 1);
 INSERT INTO Title
-VALUES(997799,'The Girl with the Dragon Tatoo','Book',57562,616, 19); 
+VALUES(997799,'The Girl with the Dragon Tatoo','Media',57562,616, 19); 
 
 INSERT INTO Customer
 VALUES('1396 Briarcliff Road Bolingbrook IL 66810','Chee Kin Chan',0,NULL,24329);
@@ -175,7 +175,7 @@ VALUES(67225,753749,63678,'Weyland-Yutani', 704);
 INSERT INTO Publisher
 VALUES(42850,733112,63815,'Ininovels', 87);
 INSERT INTO Publisher
-VALUES(43687,973722,5095,'Penguin Books', 19350);
+VALUES(43687,973722,5095,'Penguin Medias', 19350);
 INSERT INTO Publisher
 VALUES(9276,887696,29219,'Asgard', 12);
 INSERT INTO Publisher
@@ -240,48 +240,46 @@ INSERT INTO Author
 VALUES(90336,57562,'Kanye West',8, 37588);
 
 
-INSERT INTO Book
-VALUES(93423,'Brother Incorporated','Lexie Durbridge',3330, 'American Gods',1561,252663,411571610302,'Science Fiction','2002-04-05');
-INSERT INTO Book
-VALUES(21849,'Oscorp','Andrew Hale',26250, 'One Second After',86380,715810,560801259868,'Dark Fantasy','1609-02-08');
-INSERT INTO Book
-VALUES(38348,'GL Corps','James Field',19874, 'A Farewell To Arms',38735,296026,501028118699,'Historical Fiction','1811-12-04');
-INSERT INTO Book
-VALUES(81793,'Waynetech','Stephen Beeson',19971, 'What is the What',43958,243139,778452652857,'Religious Text','2013-05-09');
-INSERT INTO Book
-VALUES(88994,'Lexcorp','David Bosnich',16745, 'The Last of the Mohicans',49471,609672,385845345557,'Textbook','1963-08-28');
-INSERT INTO Book
-VALUES(9276,'Asgard','Jack Thompson',38093, 'Murder in Mesopotamia',84035,887696,874079240539,'Textbook','1989-06-23');
-INSERT INTO Book
-VALUES(905,'Themyscira','Lionel Ritchie',20946, 'Heart of Darkness',43806,884303,596976414673,'Steampunk','1387-06-01');
-INSERT INTO Book
-VALUES(21665,'Congruent','Tom Brady',35986, 'Moneyball',16369,880124,266230259337,'Self-Help','1963-12-30');
-INSERT INTO Book
-VALUES(81553,'Orthogonal','George Lopez',50881, 'How to Win Friends and Influence People',1446,594280,557268095320,'Owner Manual','2017-10-10');
-INSERT INTO Book
-VALUES(52088,'Vice','Francis Keske',65212, 'Orthodoxy',39953,801946,937891261365,'Horror','2018-07-04');
-
-
-INSERT INTO Movie
-VALUES(85940,'Nova Corps','Simon Hale',35607, 'Wonder Woman',17810,52062,5584534,'Horror','1963-08-28');
-INSERT INTO Movie
-VALUES(62672,'Stark Industries','Diana Bray',95787, 'The Shawshank Redemption',59500,307366,3412797,'Tragedy','2018-01-15');
-INSERT INTO Movie
-VALUES(67225,'Weyland-Yutani','Stuart Bryson',75917, 'Major League',63678,753749,9317720,'Comedy','2001-06-17');
-INSERT INTO Movie
-VALUES(42850,'Ininovels','Marc Butler',18679, 'A Few Good Men',63815,733112,9556108,'Action','2015-03-31');
-INSERT INTO Movie
-VALUES(43687,'Penguin Books','John Christie',55780, 'Lawrence of Arabia',5095,973722,9145626,'Courtroom Drama','1899-12-26');
-INSERT INTO Movie
-VALUES(9276,'Macmillan','Jack Medina',46295, 'Ben-Hur',29219,36325,436152,'Courtroom Drama','1959-11-18');
-INSERT INTO Movie
-VALUES(61061,'Dunder Mifflin','Anna Peck',10818, 'Stanford Prison Experiment',98406,916506,2402568,'Documentary','1992-01-30');
-INSERT INTO Movie
-VALUES(19220,'Scandal','Nigella Lawson',60430, 'The Good, The Bad, and The Ugly',93297,826524,4620185,'Biopic','1964-07-06');
-INSERT INTO Movie
-VALUES(13209,'Apart','Drake Bell',35964, 'Amistad',295,887050,7190925,'Adventure','1988-10-25');
-INSERT INTO Movie
-VALUES(32661,'20th Century Fox','Kanye West',37588, 'Blues Brothers',87771,952173,2074011,'Educational','2011-11-11');
+INSERT INTO Media
+VALUES(93423,'Brother Incorporated','Lexie Durbridge',3330, 'American Gods',1561,252663,411571610302,'Science Fiction','2002-04-05', 'Book');
+INSERT INTO Media
+VALUES(21849,'Oscorp','Andrew Hale',26250, 'One Second After',86380,715810,560801259868,'Dark Fantasy','1609-02-08', 'Book');
+INSERT INTO Media
+VALUES(38348,'GL Corps','James Field',19874, 'A Farewell To Arms',38735,296026,501028118699,'Historical Fiction','1811-12-04', 'Book');
+INSERT INTO Media
+VALUES(81793,'Waynetech','Stephen Beeson',19971, 'What is the What',43958,243139,778452652857,'Religious Text','2013-05-09', 'Book');
+INSERT INTO Media
+VALUES(88994,'Lexcorp','David Bosnich',16745, 'The Last of the Mohicans',49471,609672,385845345557,'Textbook','1963-08-28', 'Fan Fiction');
+INSERT INTO Media
+VALUES(9276,'Asgard','Jack Thompson',38093, 'Murder in Mesopotamia',84035,887696,874079240539,'Textbook','1989-06-23', 'Television Series');
+INSERT INTO Media
+VALUES(905,'Themyscira','Lionel Ritchie',20946, 'Heart of Darkness',43806,884303,596976414673,'Steampunk','1387-06-01', 'Book');
+INSERT INTO Media
+VALUES(21665,'Congruent','Tom Brady',35986, 'Moneyball',16369,880124,266230259337,'Musical','1963-12-30', 'Play');
+INSERT INTO Media
+VALUES(81553,'Orthogonal','George Lopez',50881, 'How to Win Friends and Influence People',1446,594280,557268095320,'Owner Manual','2017-10-10', 'Book');
+INSERT INTO Media
+VALUES(52088,'Vice','Francis Keske',65212, 'Orthodoxy',39953,801946,937891261365,'Horror','2018-07-04', 'Book');
+INSERT INTO Media
+VALUES(85940,'Nova Corps','Simon Hale',35607, 'Wonder Woman',17810,52062,5584534,'First Person Shooter','1963-08-28' ,'Video Game');
+INSERT INTO Media
+VALUES(62672,'Stark Industries','Diana Bray',95787, 'The Shawshank Redemption',59500,307366,3412797,'Tragedy','2018-01-15', 'Movie');
+INSERT INTO Media
+VALUES(67225,'Weyland-Yutani','Stuart Bryson',75917, 'Major League',63678,753749,9317720,'Comedy','2001-06-17', 'Movie');
+INSERT INTO Media
+VALUES(42850,'Ininovels','Marc Butler',18679, 'A Few Good Men',63815,733112,9556108,'Action','2015-03-31', 'Audiobook');
+INSERT INTO Media
+VALUES(43687,'Penguin Medias','John Christie',55780, 'Lawrence of Arabia',5095,973722,9145626,'Classical','1899-12-26', 'Album');
+INSERT INTO Media
+VALUES(9276,'Macmillan','Jack Medina',46295, 'Ben-Hur',29219,36325,436152,'Courtroom Drama','1959-11-18', 'Movie');
+INSERT INTO Media
+VALUES(61061,'Dunder Mifflin','Anna Peck',10818, 'Stanford Prison Experiment',98406,916506,2402568,'Documentary','1992-01-30', 'Magazine');
+INSERT INTO Media
+VALUES(19220,'Scandal','Nigella Lawson',60430, 'The Good, The Bad, and The Ugly',93297,826524,4620185,'Biopic','1964-07-06', 'Movie');
+INSERT INTO Media
+VALUES(13209,'Apart','Drake Bell',35964, 'Amistad',295,887050,7190925,'Adventure','1988-10-25', 'Movie');
+INSERT INTO Media
+VALUES(32661,'20th Century Fox','Kanye West',37588, 'Blues Brothers',87771,952173,2074011,'Educational','2011-11-11', 'Movie');
 
 
 INSERT INTO Department
